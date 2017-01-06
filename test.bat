277ee5f53d8f7d117b37304b572bf698490928c5
John10 Korsnes <johnkors@gmail.com>
2017-01-06 17:37:14 +0100

    Testing script finished

--- test.bat
+++ test.bat
@@ -1,17 +1,22 @@
-echo off
+@echo off
 set nuget=".\nuget\nuget.exe"
 set msbuild="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
 
-%nuget% restore .\fullframework\ConsoleDotNet45\packages.config -PackagesDirectory .\fullframework\packages\
-%nuget% restore .\fullframework\ConsoleDotNet46\packages.config -PackagesDirectory .\fullframework\packages\
+%nuget% restore .\fullframework\ConsoleDotNet45\packages.config -PackagesDirectory .\fullframework\packages\ -Verbosity quiet
+%nuget% restore .\fullframework\ConsoleDotNet46\packages.config -PackagesDirectory .\fullframework\packages\ -Verbosity quiet
 %msbuild% .\fullframework\ConsoleDotNet45\ConsoleDotNet45.csproj /t:Rebuild /p:Configuration=Release /v:quiet
 %msbuild% .\fullframework\ConsoleDotNet46\ConsoleDotNet46.csproj /t:Rebuild /p:Configuration=Release /v:quiet
 
+
+
+dotnet restore dotnetcore\ConsoleDotNetCore1.0 --verbosity Error
+dotnet restore dotnetcore\ConsoleDotNetCore1.1 --verbosity Error
+dotnet build dotnetcore\ConsoleDotNetCore1.0 
+dotnet build dotnetcore\ConsoleDotNetCore1.1 
+
+
+
 fullframework\ConsoleDotNet45\bin\Release\ConsoleDotNet45.exe
 fullframework\ConsoleDotNet46\bin\Release\ConsoleDotNet46.exe
-
-REM dotnet restore dotnetcore\ConsoleDotNetCore1.0 
-REM dotnet restore dotnetcore\ConsoleDotNetCore1.1
-
-REM dotnet run -p dotnetcore\ConsoleDotNetCore1.0
-REM dotnet run -p dotnetcore\ConsoleDotNetCore1.1
+dotnet run -p dotnetcore\ConsoleDotNetCore1.0 
+dotnet run -p dotnetcore\ConsoleDotNetCore1.1 
